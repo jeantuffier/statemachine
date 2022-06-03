ce7e181ac63f969afd18f3fe57f370c22abf3a55
jean <jean@beiningbogen.no>
2022-06-03 07:38:54 +0200

    Refactor the registration of transitions
    Instead of using this complex dsl, we can simply leverage the `when` statement
    to assign a transition to an event. It reduces code complexity and improve
    clarity.
    
    Side effect do not need to be supported the way it was after all, they can be
    a part of the view state instead.

--- src/commonMain/kotlin/com/jeantuffier/statemachine/StateMachine.kt
+++ src/commonMain/kotlin/com/jeantuffier/statemachine/StateMachine.kt
@@ -1,47 +1,65 @@
 package com.jeantuffier.statemachine
 
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
-interface StateMachine<STATE, EVENT, SIDE_EFFECT> {
-    /**
-     * The state flow holding the state of the machine.
-     */
-    val state: StateFlow<STATE>
-
-    /**
-     * The state flow holding the state of the machine.
-     */
-    val sideEffects: Flow<SIDE_EFFECT>
-
-    /**
-     * Register a transition associated to an event name.
-     * @param eventName: the name of the event that should trigger
-     * a specific transition.
-     * @param transition: the transition to register.
-     */
-    fun register(eventName: String, transition: Transition<STATE, EVENT>)
+/**
+ * A [StateMachine] should be used to extract the business logic required in a client
+ * into the common package of a KMM library.
+ *
+ * It is design to work with a unidirectional pattern. The client should use [Event]
+ * objects to represent any interaction between a user and the client. Those events
+ * are then reduced into resulting [ViewState] that the client should use to render
+ * its UI.
+ *
+ * The state machine exposes a [StateFlow] to retain its current state and be able to
+ * emit new states to the client after executing a [Transition].
+ */
+interface StateMachine<ViewState, Event> {
 
     /**
-     * Register a side effect transition associated to an event name.
-     * @param eventName: the name of the event that should trigger
-     * a specific transition.
-     * @param transition: the transition to register.
+     * Clients should collect from this state flow to receive new updates of the state.
      */
-    fun register(eventName: String, transition: TransitionWithSideEffect<STATE, EVENT, SIDE_EFFECT>)
+    val state: StateFlow<ViewState>
 
     /**
-     * When called, the state machine will check if there are any registered
-     * transition associated to that event and if it can be executed. If both
-     * conditions are met, the state machine will execute the transition.
-     * @param event: the event to use to trigger a transition.
-     * @throws : CannotApplyEventError when the event passed as parameter cannot be applied
-     * to the current state in which the state machine is.
+     * Clients should call this function whenever an [Event] is triggered by a user.
+     * @param event: The event triggered by the user.
      */
-    fun <T : EVENT> onEvent(eventName: String, event: T)
+    suspend fun reduce(event: Event)
+}
 
-    /**
-     * Clean up resources.
-     */
-    fun destroy()
+/**
+ * This class facilitate the implementation of a [StateMachine].
+ * It already contains a [MutableStateFlow] to update the state machine state
+ * and is used to override [StateMachine.state].
+ * It also has a [StateMachineBuilder.onEvent] extension allowing simple
+ * matching between events and transitions.
+ *
+ * @param initialValue: the value used to initialize the state.
+ * @param reducer: a function matching events with transition. The easiest way
+ * to do so is by using a `when` statement.
+ */
+internal class StateMachineBuilder<ViewState, Event>(
+    initialValue: ViewState,
+    private val reducer: suspend StateMachineBuilder<ViewState, Event>.(Event) -> Unit
+) : StateMachine<ViewState, Event> {
+    internal val _state = MutableStateFlow(initialValue)
+    override val state = _state.asStateFlow()
+
+    override suspend fun reduce(event: Event) = reducer(event)
+}
+
+/**
+ * A helper function checking if a transition can be executed.
+ */
+internal suspend fun <ViewState, Event, T : Event> StateMachineBuilder<ViewState, Event>.onEvent(
+    transition: Transition<ViewState, T>,
+    event: T,
+) {
+    if (transition.isExecutable(state.value)) {
+        _state.update { transition.execute(state.value, event) }
+    }
 }
--- src/commonMain/kotlin/com/jeantuffier/statemachine/Transition.kt
+++ src/commonMain/kotlin/com/jeantuffier/statemachine/Transition.kt
@@ -1,32 +1,44 @@
 package com.jeantuffier.statemachine
 
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-
-data class Transition<STATE, EVENT>(
-    /**
-     * A lambda used to determine if the transition should be executed or not.
-     * It has a state parameter as input, it should be the current state of the
-     * state machine. A typical use case would be to check if data has already
-     * been loaded before executing the transition if its purpose is to load data.
-     */
-    val isExecutable: IsExecutable<STATE>,
-
-    /**
-     * A lambda executing the logic to transition the state machine from its
-     * current state to the next one.
-     */
-    val execute: Execute<EVENT, STATE>,
-)
-
-fun interface IsExecutable<STATE> {
-    operator fun invoke(state: STATE): Boolean
+/**
+ * A [Transition] abstracts the logical operations required for changing the
+ * state of the machine into its nest state, based on a specific [Event].
+ */
+interface Transition<State, Event> {
+    fun isExecutable(state: State): Boolean
+    suspend fun execute(state: State, event: Event): State
 }
 
-fun interface Execute<EVENT, STATE> {
-    suspend operator fun invoke(event: EVENT, state: MutableStateFlow<STATE>): Unit
+/**
+ * Abstracts the logic to check if a given transition should be executed.
+ * A typical use case would be to check if the current state represents a loading
+ * state before executing a new transition.
+ */
+fun interface IsExecutable<State> {
+
+    /**
+     * @param state: the current state of the state machine.
+     */
+    operator fun invoke(state: State): Boolean
 }
 
-fun interface ExecuteSideEffect<EVENT, SIDE_EFFECT> {
-    suspend operator fun invoke(event: EVENT, state: MutableSharedFlow<SIDE_EFFECT>): Unit
+/**
+ * Abstracts the logic to transition the state machine from its current state to
+ * the next one.
+ */
+fun interface Execute<State, Event> {
+
+    /**
+     * @param state: the current state of the state machine.
+     * @param event: the event that triggers the transition.
+     */
+    suspend operator fun invoke(state: State, event: Event): State
+}
+
+internal class TransitionBuilder<State, Event>(
+    private val predicate: IsExecutable<State>,
+    private val execution: Execute<State, Event>
+) : Transition<State, Event> {
+    override fun isExecutable(state: State) = predicate(state)
+    override suspend fun execute(state: State, event: Event): State = execution(state, event)
 }
