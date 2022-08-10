94182830e37c28313ef02bfe9f31e5091626c65d
jean <jean@beiningbogen.no>
2022-08-10 11:27:58 +0200

    Update the framework to reuse transitions across view states

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
-    kotlin("multiplatform") version "1.6.10"
-    id("com.rickclephas.kmp.nativecoroutines") version "0.12.2"
+    kotlin("multiplatform") version "1.7.10"
     id("maven-publish")
 }
 
 group = "com.jeantuffier"
-version = "2.0.7"
+version = "2.1.0-alpha1"
 
 repositories {
     google()
