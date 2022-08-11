58302085aae65b191676fa9872ab882e1a4ea05f
jean <jean@beiningbogen.no>
2022-08-11 10:52:31 +0200

    Configure the ksp processor

---
+++ processor/build.gradle.kts
@@ -0,0 +1,23 @@
+
+val kspVersion: String by project
+
+plugins {
+    kotlin("multiplatform")
+}
+
+group = "com.jeantuffier"
+version = "0.1.0-SNAPSHOT"
+
+kotlin {
+    jvm()
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation("com.squareup:javapoet:1.12.1")
+                implementation("com.google.devtools.ksp:symbol-processing-api:$kspVersion")
+            }
+            kotlin.srcDir("src/main/kotlin")
+            resources.srcDir("src/main/resources")
+        }
+    }
+}
